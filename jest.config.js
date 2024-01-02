1212098cdf1ebc8401fd0075c706e25257fa6d98
Oriol Carbo <oriol.carbo@gmail.com>
2024-01-02 20:01:34 +0100

    add config for jest and ts

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
