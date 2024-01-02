7b5366a761ff1b206aa9f0f4c9fef287e9e4c436
Oriol Carbo <oriol.carbo@gmail.com>
2024-01-02 17:16:22 +0100

    update api call with random type

--- src/api/BoredAPI.ts
+++ src/api/BoredAPI.ts
@@ -3,9 +3,15 @@ import {Activity} from '../types/activityTypes';
 
 const BASE_URL = 'https://www.boredapi.com/api';
 
-export const getRandomActivity = async (): Promise<Activity | undefined> => {
+export const getRandomActivity = async (type: string) => {
+  let url = `${BASE_URL}/activity`;
+
+  if (type !== 'random') {
+    url += `?type=${type}`;
+  }
+
   try {
-    const response = await axios.get(`${BASE_URL}/activity`);
+    const response = await axios.get(url);
     return response.data as Activity;
   } catch (error) {
     console.error(error);
